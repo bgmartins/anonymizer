2182562d73dd345ada5890f2f64682f4a8b57b23
Bruno Martins <bgmartins@users.noreply.github.com>
2022-12-26 12:50:52 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 
 mkdir -p ~/.streamlit/
 
+python -m spacy download en_core_web_sm-2.2.0
+python -m spacy download en_core_web_sm-2.2.0
+python -m spacy download en_core_web_sm-2.2.0
+
 echo "\
 [server]\n\
 headless = true\n\
