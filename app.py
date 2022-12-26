735cd19889748be5ac4cdb9acbb73129ee11e3f2
Bruno Martins <bgmartins@users.noreply.github.com>
2022-12-26 14:17:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from annotated_text import annotated_text
 
 @st.cache(show_spinner=False, allow_output_mutation=True, suppress_st_warning=True)
 def load_models():
+    patterns = [
+                {
+                    "label": "EMAIL", "pattern": [ {"TEXT": {"REGEX": "\b((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)\b"}} ]
+                }
+    ]
     french_model = spacy.load("fr_dep_news_trf")
     english_model = spacy.load("en_core_web_trf")
     portuguese_model = spacy.load("pt_core_news_lg")
+    french_model.add_pipe("entity_ruler").add_patterns(patterns)
+    english_model.add_pipe("entity_ruler").add_patterns(patterns)
+    portuguese_model.add_pipe("entity_ruler").add_patterns(patterns)
     models = {"pt": portuguese_model, "en": english_model, "fr": french_model }
     return models
 
@@ -19,6 +27,8 @@ def process_text(doc, selected_entities, anonymize=False):
             tokens.append((token.text, "Location", "#fda"))
         elif (token.ent_type_ == "ORG") & ("ORG" in selected_entities):
             tokens.append((token.text, "Organization", "#afa"))
+        elif (token.ent_type_ == "EMAIL") & ("EMAIL" in selected_entities):
+            tokens.append((token.text, "Email", "#aaf"))
         else:
             tokens.append(" " + token.text + " ")
     if anonymize:
@@ -36,8 +46,8 @@ models = load_models()
 selected_language = st.sidebar.selectbox("Select a language", options=["pt", "en", "fr"])
 selected_entities = st.sidebar.multiselect(
     "Select the entities you want to detect",
-    options=["LOC", "PER", "ORG"],
-    default=["LOC", "PER", "ORG"],
+    options=["LOC", "PER", "ORG", "EMAIL"],
+    default=["LOC", "PER", "ORG", "EMAIL"],
 )
 selected_model = models[selected_language]
 
